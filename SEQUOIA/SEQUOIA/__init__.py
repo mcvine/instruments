cb50e4911d2acf94ac51cd440fe61b3fc9d7e79a
Jiao Lin <linjiao@ornl.gov>
2018-02-14 09:19:21 -0500

    Refs mcvine/mcvine#269. fixing files in mcvine.instruments.SEQUOIA

--- SEQUOIA/SEQUOIA/__init__.py
+++ SEQUOIA/SEQUOIA/__init__.py
@@ -1,19 +1,15 @@
 # -*- Python -*-
 #
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#
-#                                   Jiao Lin
-#                      California Institute of Technology
-#                      (C) 2006-2011  All Rights Reserved
-#
-# {LicenseText}
-#
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-#
 
 from . import mantid2mcvine # make sure mantid IDF is there
 
-# version
-__id__ = "$Id$"
+
+# constants
+L_Beam = 19.9                  # Moderator to neutron recorder distance.
+                               # This has to match the neutron recorder position .../etc/sequoia_moderator2sample/sequoia_moderator2sample.pml
+
+L_BeamEnd2Sample = 0.15        # End of beam to sample. This is used in mcvine.workflow.cli
+L_Mod2Sample = 19.9 + 0.15     # Moderator sample distance. This has to match the mantid IDF and the mantid template nexus file.
+                               # SEQ_virtual_Definition_12292017.xml and SEQ_virtual_template_12292017.nxs
 
 # End of file 
