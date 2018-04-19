244ac1d5cff8aeac496d5c7ff24e7e00d6038702
Jiao Lin <linjiao@ornl.gov>
2018-04-18 22:20:40 -0400

    mcvine/ui#21. ARCS/beam_postprocessing.py: save T0

--- ARCS/ARCS/beam_postprocessing.py
+++ ARCS/ARCS/beam_postprocessing.py
@@ -19,6 +19,19 @@ and generate better outputs in "out".
 
 """
 
+# These parameters should match the positions in arcs-moderator2sample.
+# The default values for them are in .../etc/arcs_moderator2sample/arcs_moderator2sample.pml
+# distance from moderator to monitor1, unit meter
+LM1 = 11.831
+# distance from moderator to monitor2
+LM2 = 18.5
+
+
+# distance to sample.
+# This should match .../etc/arcs_analyze_beam/arcs_analyze_beam.pml
+LSAMPLE = 13.6
+
+
 import os, time
 
 
@@ -37,6 +50,12 @@ def run(m2sout, out, Ei):
     moveNeutronsToOutputDir(m2sout, out)
     # compute average incident energy at sample
     energy = computeAverageEnergy(out)
+    # compute average tof at sample
+    tof = computeAverageTof(out)
+    # compute emission time
+    from mcni.utils import conversion
+    vi = conversion.e2v(energy)
+    t0 = tof - LSAMPLE/vi
     # compute fwhm of energy spetrum at sample
     fwhm = computeFWHM(out)
     fwhm *= 1e6 # convert to microsecond
@@ -45,6 +64,8 @@ def run(m2sout, out, Ei):
         'flux': '%s counts per 34kJ pulse' % flux,
         'average energy': '%s meV' % energy,
         'tof fwhm': '%s microsecond' % fwhm,
+        "emission time": '%s microsecond' % (t0*1e6),
+        'average tof': '%s microsecond' % (tof*1e6),
         }
     open(os.path.join(out, 'props.json'), 'w').write(str(props))
     return
@@ -71,6 +92,13 @@ def computeAverageEnergy(out):
     h = load(os.path.join(out, 'ienergy.h5'), 'ienergy')
     e = (h.energy * h.I).sum()/h.I.sum()
     return e
+
+
+def computeAverageTof(out):
+    from histogram.hdf import load
+    h = load(os.path.join(out, 'itof.h5'), 'itof')
+    tof = (h.tof * h.I).sum()/h.I.sum()
+    return tof
 
 
 def computeFWHM(out):
@@ -125,8 +153,7 @@ def computeFocusedSpectraForRealMonitors(E, m2sout, out):
     import histogram.hdf as hh, histogram as H
 
     m1 = hh.load(os.path.join(m2sout, 'mon1-tof.h5'), 'I(tof)')
-    L1 = 11.831
-    t1 = L1/v #* second
+    t1 = LM1/v #* second
     m1p = m1[(t1*0.9, t1*1.1)]
     m1pc = H.histogram('I(tof)', m1p.axes(), data=m1p.I, errors=m1p.E2)
     m1pc.setAttribute('title', 'Monitor 1 I(tof)')
@@ -134,8 +161,7 @@ def computeFocusedSpectraForRealMonitors(E, m2sout, out):
     hh.dump(m1pc, os.path.join(out, 'mon1-itof-focused.h5'), '/', 'c')
     
     m2 = hh.load(os.path.join(m2sout, 'mon2-tof.h5'), 'I(tof)')
-    L2 = 18.5
-    t2 = L2/v #* second
+    t2 = LM2/v #* second
     m2p = m2[(t2*0.9, t2*1.1)]
     m2pc = H.histogram('I(tof)', m2p.axes(), data=m2p.I, errors=m2p.E2)
     m2pc.setAttribute('title', 'Monitor 2 I(tof)')
