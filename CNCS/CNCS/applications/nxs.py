d270e5b60e5cb38d80cfeb32c3f90716944229d1
Jiao Lin <linjiao@ornl.gov>
2018-05-10 15:07:56 -0400

    Refs #8. updated CNCS/applications/nxs.py

--- CNCS/CNCS/applications/nxs.py
+++ CNCS/CNCS/applications/nxs.py
@@ -3,16 +3,19 @@
 #   Jiao Lin
 #
 
-def populate_Ei_data(beam_outdir, nxs):
-    import json, os
-    props_path = os.path.join(beam_outdir, 'props.json')
-    s = open(props_path).read()
-    s = s.replace("'", '"') # ' -> "
-    props = json.loads(s)
-    Ei, unit = props['average energy'].split()
-    Ei = float(Ei)
-    assert unit == 'meV'
-    setEnergyRequest(nxs, Ei)
+def populate_Ei_data(sim_out, nxs):
+    import ast, os
+    props = ast.literal_eval(open(os.path.join(sim_out, 'props.json')).read())
+    Ei, unit = props['average energy'].split(); assert unit=='meV'
+    t0, unit = props['emission time'].split(); assert unit=='microsecond'
+    setEnergyRequest(nxs, float(Ei))
+    from mantid import simpleapi as msa
+    if isinstance(nxs, unicode):
+        nxs = nxs.encode('utf-8')
+    ws = msa.Load(nxs)
+    msa.AddSampleLog(ws, LogName='mcvine-Ei', LogText=str(Ei), LogType='Number')
+    msa.AddSampleLog(ws, LogName='mcvine-t0', LogText=str(t0), LogType='Number')
+    msa.SaveNexus(ws, nxs)
     return
 
 
@@ -33,6 +36,15 @@ def setEnergyRequest(path, Ei):
     er['value'][0] = Ei
     f.close()
     return
+
+
+def reduce(nxsfile, qaxis, outfile, use_ei_guess=False, ei_guess=None, eaxis=None, tof2E=True, ibnorm='ByCurrent', t0_guess=None):
+    from ...ARCS.applications.nxs import reduce
+    reduce(
+        nxsfile, qaxis, outfile,
+        use_ei_guess=use_ei_guess, ei_guess=ei_guess,
+        eaxis=eaxis, tof2E=tof2E, ibnorm=ibnorm,
+        t0_guess=t0_guess, use_monitors=False)
 
 
 # End of file 
