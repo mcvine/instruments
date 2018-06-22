55a377a958771b23754ede2afd763efeebf48140
Jiao Lin <linjiao@ornl.gov>
2018-06-21 21:02:19 -0400

    Refs mcvine/ui#31. populate_metadata: add Ei and t0 into log

--- HYSPEC/HYSPEC/applications/nxs.py
+++ HYSPEC/HYSPEC/applications/nxs.py
@@ -4,11 +4,24 @@
 #
 
 def populate_metadata(sim_out, nxs, sample, detector):
+    if isinstance(nxs, unicode):
+        nxs = nxs.encode('utf-8')
     import h5py
     f = h5py.File(nxs, 'a')
     entry = f['entry']
     from mcvine.instruments.HYSPEC.nxs.raw import populateMetadata
     populateMetadata(entry, sim_out, sample, detector)
+    f.close()
+    # add log entries
+    import ast, os
+    props = ast.literal_eval(open(os.path.join(sim_out, 'props.json')).read())
+    Ei, unit = props['average energy'].split(); assert unit=='meV'
+    t0, unit = props['emission time'].split(); assert unit=='microsecond'
+    from mantid import simpleapi as msa
+    ws = msa.Load(nxs)
+    msa.AddSampleLog(ws, LogName='mcvine-Ei', LogText=str(Ei), LogType='Number')
+    msa.AddSampleLog(ws, LogName='mcvine-t0', LogText=str(t0), LogType='Number')
+    msa.SaveNexus(ws, nxs)
     return
 
 # End of file
