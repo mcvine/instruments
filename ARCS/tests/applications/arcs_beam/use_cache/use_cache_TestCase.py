265736e130404c1f905b882e3607ad4b72f716f2
Jiao Lin <jiao.lin@gmail.com>
2020-10-12 22:50:01 -0400

    new test to catch the unicode bug in mcvine.cli._provenance

---
+++ ARCS/tests/applications/arcs_beam/use_cache/use_cache_TestCase.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+import subprocess as sp, os, shlex
+
+import unittest
+class TestCase(unittest.TestCase):
+
+    def test(self):
+        workdir = os.path.abspath(os.path.dirname(__file__))
+        cmd = "mcvine instruments arcs beam --keep-in-cache --use-cache -E=70 --ncount=1e6 --nodes=2"
+        sp.check_call(shlex.split(cmd), shell=False, cwd=workdir)
+        return
+
+if __name__ == '__main__': unittest.main()
+
+# End of file
