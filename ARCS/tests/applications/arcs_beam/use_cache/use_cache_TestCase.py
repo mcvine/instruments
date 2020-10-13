a117860c7407276cd7099f524abc6117b7fd5201
Jiao Lin <jiao.lin@gmail.com>
2020-10-13 09:42:55 -0400

    use_cache test: increase ncount so that it is easier to pass

--- ARCS/tests/applications/arcs_beam/use_cache/use_cache_TestCase.py
+++ ARCS/tests/applications/arcs_beam/use_cache/use_cache_TestCase.py
@@ -7,7 +7,7 @@ class TestCase(unittest.TestCase):
 
     def test(self):
         workdir = os.path.abspath(os.path.dirname(__file__))
-        cmd = "mcvine instruments arcs beam --keep-in-cache --use-cache -E=70 --ncount=1e6 --nodes=2"
+        cmd = "mcvine instruments arcs beam --keep-in-cache --use-cache -E=70 --ncount=4e6 --nodes=2"
         sp.check_call(shlex.split(cmd), shell=False, cwd=workdir)
         return
 
